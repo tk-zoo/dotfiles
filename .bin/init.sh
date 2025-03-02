ad2086d526d1b36be0362c0fa41307fcd05797e1
tk-zoo <takeya@rehabforjapan.com>
2025-03-02 09:42:21 +0900

    homebrew 修正

--- .bin/init.sh
+++ .bin/init.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/zsh
 
 if [ "$(uname)" != "Darwin" ] ; then
 	echo "Not macOS!"
 	exit 1
 fi
 
-# Set zsh
-chsh -s /bin/zsh
+# Set Shell(zsh)
+# chsh -s /bin/zsh
 
 # Install Rosetta 2 for Apple Silicon
 if [ "$(uname -m)" = "arm64" ] ; then
@@ -17,7 +17,7 @@ fi
 xcode-select --install > /dev/null
 
 # Install brew
-/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
 if [ "$(uname -m)" = "arm64" ] ; then
   (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/${USER}/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 1. OSはアップデートして最新版にしておく
 2. リポジトリを新しいMacにZipダウンロードする
+	- curl -L -o main.zip https://github.com/tk-zoo/dotfiles/archive/refs/heads/main.zip
 3. ダウンロードしたZipを好みのディレクトリに移動する
 4. dotfile/.bin/以下の.shファイルに対して、chmod +xして実行権限をつける
+	- chmod +x .bin/*.sh
 5. dotfile/でmakeする
 	- make allで、init.sh　brew.sh　defaults.sh　setup.sh　other_apps.shが順次実行される
 	- 以下で個別実行が可能
