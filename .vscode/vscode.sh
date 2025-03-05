b321a5605a77442d69c71fb85d9ead4470097dbf
tk-zoo <takeya@rehabforjapan.com>
2025-03-05 16:09:28 +0900

    modify

--- .vscode/vscode.sh
+++ .vscode/vscode.sh
@@ -5,11 +5,11 @@ VSCODE_SETTING_PATH="${HOME}/Library/Application\ Support/Code/User/settings.jso
 
 if [[ ! -L "${VSCODE_SETTING_PATH}" ]]; then
   echo "Linking settings.json to vscode..."
-  ln -fsvn "${VSCODE_SETTING_PATH}" "${SCRIPT_DIR}/settings.json" || {
+  ln -fsvn "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_PATH}" || {
     echo "Error creating symbolic link." >&2
     exit 1
   }
-  echo "Symbolic link created: ${SCRIPT_DIR}/settings.json" -> ${VSCODE_SETTING_PATH}
+  echo "Symbolic link created: ${VSCODE_SETTING_PATH} -> ${SCRIPT_DIR}/settings.json"
 else
   echo "VSCode settings.json link already exists."
 fi
